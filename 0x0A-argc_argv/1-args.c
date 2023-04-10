38e112f447abe11e1392d458cc2cbe1bd885b999
MFVin <gichangokk@gmail.com>
2023-04-10 13:08:38 -0400

    C - argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints number passed
+ * @argc: argument
+ * @argv: argument
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
