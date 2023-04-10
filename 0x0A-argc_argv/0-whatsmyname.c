4257a1772d2ffd5e62788d0966e099a713130e25
MFVin <gichangokk@gmail.com>
2023-04-10 12:58:01 -0400

    01

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: prints the name of the program
+ * @argc: number of command line arguements
+ * @argv: an array of command line arguements
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", *argv);
+return (0);
+}:
